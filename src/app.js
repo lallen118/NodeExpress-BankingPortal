f2a73a56ed9585c3d1866b30c0febebf365f3fd1
lallen118 <github@louisallen.com>
2019-01-05 16:47:33 -0700

    Completed the module

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-
 const app = express();
 
 app.set('views', path.join(__dirname + '/views'));
@@ -10,6 +9,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+const accountData = fs.readFileSync(path.join(__dirname,'json','accounts.json'),'UTF8');
+const accounts =  JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname,'json','users.json'),'UTF8');
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts }));
+
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => console.log('PS Project Running'));
